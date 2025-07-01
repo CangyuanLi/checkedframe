df3fcd16aabbc7057835a5e8684ec8b466f7c1e2
Cangyuan Li <everest229@gmail.com>
2025-06-30 22:45:01 -0700

    chore: error handling in update_docs

--- update_docs.py
+++ update_docs.py
@@ -2,5 +2,5 @@ import subprocess
 
 if __name__ == "__main__":
     print("uploading docs")
-    subprocess.run(["sphinx-build", "docs/", "docs/_build/html/"])
-    subprocess.run(["ghp-import", "-n", "-p", "-f", "docs/_build/html/"])
+    subprocess.run(["sphinx-build", "docs/", "docs/_build/html/"], check=True)
+    subprocess.run(["ghp-import", "-n", "-p", "-f", "docs/_build/html/"], check=True)
